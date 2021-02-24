0a7d2398467e1fba644088a743a0eec876eba463
MarcosVel <marcos.veloso@sempreceub.com>
2021-02-23 23:51:15 -0300

    add post request

--- api/src/server.ts
+++ api/src/server.ts
@@ -2,8 +2,12 @@ import express from 'express';
 
 const app = express();
 
-app.get("/users", (request, response) => {
-  return response.json({message: "Hello World NLW"});
+app.get("/", (request, response) => {
+  return response.json({ message: "Hello World NLW" });
+});
+
+app.post("/", (request, response) => {
+  return response.json({ message: "Os dados foram salvos com sucesso!" });
 });
 
 app.listen(3333, () => console.log("Server is running!"));
