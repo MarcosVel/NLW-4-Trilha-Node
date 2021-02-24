b46a4d742090f4aabeff5141952398a0b8693af0
MarcosVel <marcos.veloso@sempreceub.com>
2021-02-23 22:57:33 -0300

    creating api

---
+++ api/src/server.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express();
+
+app.get("/users", (request, response) => {
+  return response.json({message: "Hello World NLW"});
+});
+
+app.listen(3333, () => console.log("Server is running!"));
