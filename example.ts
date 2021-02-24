6337d4862fc01261858af9741301098880dee766
MarcosVel <marcos.veloso@sempreceub.com>
2021-02-23 22:23:38 -0300

    example req

---
+++ example.ts
@@ -0,0 +1,21 @@
+interface DadosDeEnvioEmail {
+  para: string;
+  id: string;
+  assunto: string;
+  texto: string;
+}
+
+function enviarEmail({ para, id, assunto, texto }: DadosDeEnvioEmail) {
+  console.log(para, id, assunto, texto);
+}
+
+class EnviarEmailParaUsuario {
+  send() {
+    enviarEmail({
+      para: 'dani@gmail.com',
+      id: '9899',
+      assunto: 'Olá!',
+      texto: 'Tudo bem?'
+    });
+  }
+}
